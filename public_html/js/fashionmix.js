9eb5d586c2346162373778064365ade31d9a9c87
dimitris <dimitris@ubuntu>
2013-02-17 11:21:21 +0200

    update 3

--- public_html/js/fashionmix.js
+++ public_html/js/fashionmix.js
@@ -1,3 +1,7 @@
+(function(c,q){var m="data:image/gif;base64,R0lGODlhAQABAIAAAAAAAP///ywAAAAAAQABAAACAUwAOw==";c.fn.imagesLoaded=function(f){function n(){var b=c(j),a=c(h);d&&(h.length?d.reject(e,b,a):d.resolve(e));c.isFunction(f)&&f.call(g,e,b,a)}function p(b){k(b.target,"error"===b.type)}function k(b,a){b.src===m||-1!==c.inArray(b,l)||(l.push(b),a?h.push(b):j.push(b),c.data(b,"imagesLoaded",{isBroken:a,src:b.src}),r&&d.notifyWith(c(b),[a,e,c(j),c(h)]),e.length===l.length&&(setTimeout(n),e.unbind(".imagesLoaded",
+p)))}var g=this,d=c.isFunction(c.Deferred)?c.Deferred():0,r=c.isFunction(d.notify),e=g.find("img").add(g.filter("img")),l=[],j=[],h=[];c.isPlainObject(f)&&c.each(f,function(b,a){if("callback"===b)f=a;else if(d)d[b](a)});e.length?e.bind("load.imagesLoaded error.imagesLoaded",p).each(function(b,a){var d=a.src,e=c.data(a,"imagesLoaded");if(e&&e.src===d)k(a,e.isBroken);else if(a.complete&&a.naturalWidth!==q)k(a,0===a.naturalWidth||0===a.naturalHeight);else if(a.readyState||a.complete)a.src=m,a.src=d}):
+n();return d?d.promise(g):g}})(jQuery);
+
 var x1, y1, h1, w1;
 var jcrop_api, jcrop_api2;
 d = new Date();
@@ -166,29 +170,37 @@ $(function() {
   });
 
 $(".btnCrop").click(function(){
-    
+    $(".btnCrop").text("please wait..");
     $.ajax({
              url: "srvscripts/crop.php",
             data: {xi1:x1, yi1: y1, wi1:w1, hi1:h1, index: 1},
             dataType: "text"
           }).done (function(data) {
                    
-                      $('#mix-image1').attr('src', "srvscripts/"+data+"?"+d.getTime());
-                          }
+                    $('#mix-image1').attr('src', "srvscripts/"+data+"?"+d.getTime());
+                    $("#mix-image1").imagesLoaded(function() {
+                        $(".btnCrop").text("Crop"); 
+                    });
+                 }
+                    
              ) ;
      
+
   }
 );
 
 $(".btnCrop2").click(function(){
-    
+    $(".btnCrop2").text("please wait..");
     $.ajax({
              url: "srvscripts/crop.php",
             data: {xi1:x1, yi1: y1, wi1:w1, hi1:h1, index: 2},
             dataType: "text"
           }).done (function(data) {
                       $('#mix-image2').attr('src', "srvscripts/"+data+"?"+d.getTime());
-                          }
+                      $("#mix-image2").imagesLoaded(function() {
+                           $(".btnCrop2").text("Crop"); 
+                       }); 
+                    }
              ) ;
      
   }
@@ -196,6 +208,7 @@ $(".btnCrop2").click(function(){
  
  $("#grab").click(function() {
      var img_url = $("#enter_url").attr('value');
+     $("#grab").text(" please wait...");
      $("#ajax-loader").show();
      $.ajax({
              url: "srvscripts/get_images.php",
@@ -216,18 +229,23 @@ $(".btnCrop2").click(function(){
                        $("#ajax-loader").hide(); 
                        $('#mix-image1').attr('src', "srvscripts/"+image1);
                        $("#msgLoad1").hide(100);
+                  $("#mix-image1").imagesLoaded(function() {
                        $("#img1-selector").fadeOut(200);
+                       $("#grab").text("Load");
                        $("#cropme").removeAttr('disabled');
                        $( "#slider1" ).slider("option", "disabled", false);
                        showInstructResize(1);
-                   }
-            }) ;
+                        });
+                       }
+                   });
+           
    
     });
 
 $("#grab2").click(function(event) {
     event.preventDefault();
      var img_url = $("#enter_url2").attr('value');
+     $("#grab2").text(" please wait...");
      $("#ajax-loader2").show();
      $.ajax({
              url: "srvscripts/get_images.php",
@@ -248,10 +266,13 @@ $("#grab2").click(function(event) {
                        $("#ajax-loader2").hide();
                        $('#mix-image2').attr('src', "srvscripts/"+image2);
                        $("#msgLoad2").hide(100);
+                   $("#mix-image2").imagesLoaded(function() {
                        $("#img2-selector").fadeOut(200);
+                       $("#grab2").text("Load");
                        $("#cropme2").removeAttr('disabled');
                        $( "#slider2" ).slider("option", "disabled", false);
-                       showInstructResize(2);
+                       showInstructResize(2); 
+                   });
                  }
             
             }) ;
@@ -287,4 +308,7 @@ window.onbeforeunload = function(e) {
 */
 
 });
+
+
+
 
